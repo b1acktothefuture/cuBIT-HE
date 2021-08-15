a550f6e334b39f9ef833b41e03e4b35861107a0b
b1acktothefuture <mutyalwarsm19.extc@coep.ac.in>
2021-08-15 22:02:06 +0530

    .

--- includes/kernel.cuh
+++ includes/kernel.cuh
@@ -5,13 +5,10 @@
 typedef uint4 u128;
 typedef u128 big;
 
-__device__ void add_uint128 (u128* a,u128 augend);
-__device__ u128 mul_uint128 (u128 a, u128 b);
-__device__ void sub_uint128 (u128* a,u128 augend);
-__global__ void gpu_sparse_mult(big *a,big *b, big *c, int m, int k,uint bits,big q);
-__global__ void gpu_add(uint size,big q, big *x, big *y) ;
-
-
-
+__device__ void add_uint128(u128 *a, u128 augend);
+__device__ u128 mul_uint128(u128 a, u128 b);
+__device__ void sub_uint128(u128 *a, u128 augend);
+__global__ void gpu_sparse_mult(big *a, big *b, big *c, int m, int k, uint bits, big q);
+__global__ void gpu_add(uint size, big q, big *x, big *y);
 
 #endif
